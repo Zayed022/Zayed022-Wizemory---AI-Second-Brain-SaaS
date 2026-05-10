74eef807ad8618cf1ce080d26c74efd0192aee91
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2026-05-10 20:41:14 +0530

    Fix middleware.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,8 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: ['/dashboard/', '/api/', '/auth/'] },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/dashboard/', '/api/', '/auth/'],
+    },
     sitemap: 'https://www.wizemory.com/sitemap.xml',
-    host: 'https://www.wizemory.com',
   }
 }
--- middleware.ts
+++ middleware.ts
@@ -40,7 +40,7 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|.*\\..*|api|trpc|sitemap.xml|robots.txt).*)',
     '/(api|trpc)(.*)',
   ],
 }
