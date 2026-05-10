a9385cb4ef9c47f3dfcb183c760a92478e09acc8
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2026-05-10 20:00:32 +0530

    Worked on sitemap file

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/', disallow: ['/dashboard/', '/api/', '/auth/'] },
-    sitemap: 'https://wizemory.com/sitemap.xml',
-    host: 'https://wizemory.com',
+    sitemap: 'https://www.wizemory.com/sitemap.xml',
+    host: 'https://www.wizemory.com',
   }
 }
